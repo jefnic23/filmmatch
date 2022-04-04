90aab1f86fafe902c9a0f0ec325395690483253f
Jeff <jefnic23@gmail.com>
2022-04-04 13:06:04 -0400

    assert local time

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,15 @@
 import random
 import pandas as pd
-from datetime import date
+from datetime import datetime, timezone
 from flask import render_template
 from app.game import *
 from app import app
 
 @app.route('/') 
 def index():
-    year = int(date.today().timetuple().tm_year)
-    day = int(date.today().timetuple().tm_yday) - 1
+    today = datetime.now(timezone.utc).astimezone()
+    year = int(today.today().timetuple().tm_year)
+    day = int(today.today().timetuple().tm_yday) - 1
     cast = pd.read_csv('app/static/data/cast.csv')
     movies = pd.read_csv('app/static/data/movies.csv')
     actors = pd.read_csv('app/static/data/actors.csv')
@@ -22,16 +23,16 @@ def index():
             random.seed(year+day)
             if current['category'] == 'primaryTitle':
                 df = cast[cast['primaryTitle'] == current['name']]
-                n = [v.to_dict() for k, v in actors[actors['name'].isin(df['nconst'].tolist())].iterrows() if v['name'] not in [x["name"] for y in rollcall for x in y]]    
+                n = [v.to_dict() for k, v in actors[actors['name'].isin(df['nconst'].tolist())].iterrows() if v['name'] not in [x["name"] for y in rollcall for x in y] and v['name'] != first['name']]    
             else:
                 df = cast[cast['nconst'] == current['name']]
-                n = [v.to_dict() for k, v in movies[movies['name'].isin(df['primaryTitle'].tolist())].iterrows() if v['name'] not in [x["name"] for y in rollcall for x in y]]
+                n = [v.to_dict() for k, v in movies[movies['name'].isin(df['primaryTitle'].tolist())].iterrows() if v['name'] not in [x["name"] for y in rollcall for x in y] and v['name'] != first['name']]
             current = random.choices(n, weights=[i['weight'] for i in n], k=1)[0]
             current['answer'] = True
-            rollcall.append(getWrongAnswers(current, cast, actors, movies, year+day+current['weight']))
+            rollcall.append(getWrongAnswers(current, cast, actors, movies, year+day+current['weight']+len(rollcall)))
         except:
             break
-    return render_template('index.html', first=first, data=rollcall)
+    return render_template('index.html', first=first, data=rollcall, today=today)
 
 if __name__ == '__main__':
     app.run()
--- app/static/main.js
+++ app/static/main.js
@@ -1,6 +1,31 @@
 const current = document.getElementById("current");
 const answers = document.getElementById("answers");
 
+function getStats() {
+    let statistics = localStorage.getItem("statistics");
+    return (statistics == true) ? JSON.parse(statistics) : {'lastPlayed': "Never", 'daysPlayed': 0, 'rightAnswers': 0, 'avgAnswers': 0, 'avgPct': 0};
+}
+
+function getGameState(lastPlayed, data) {
+    let game_state = localStorage.getItem("game_state");
+    return (game_state == false || compDay() > lastPlayed) ? {"gameLength": data.length, "answers": 0, "compPct": 0, "status": "in_progress"} : JSON.parse(game_state);
+}
+
+function updateStats(stats, lastPlayed, rightAnswers) {
+    return ;
+}
+
+function updateGameState(game_state, answers) {
+    return ;
+}
+
+function compDay() {
+    let today = new Date()
+    let offset = today.getTimezoneOffset();
+    today = new Date(today.getTime() - (offset*60*1000));
+    return today.toISOString().split('T')[0];
+}
+
 function startRound(c) {
     displayCurrent(c);
     for (let i = 0; i < answers.children.length; i++) {
@@ -10,8 +35,12 @@ function startRound(c) {
 
 function displayCurrent(c) {
     let caption = (c.category == "primaryTitle") ? "also starred..." : "also starred in...";
-    current.children[0].innerHTML = c.name;
-    current.children[1].innerHTML = caption;
+    current.children[1].innerHTML = c.name;
+    current.children[2].innerHTML = caption;
+}
+
+function updateScore() {
+    return current.children[0].innerHTML = parseInt(current.children[0].innerHTML) + 1;
 }
 
 function getAnswer(guess) {
@@ -22,7 +51,7 @@ function checkAnswer(el) {
     let guess = el.innerHTML;
     let answer = data.shift().filter(getAnswer)[0];
     if (guess == answer.name) {
-        alert("Correct!");
+        updateScore();
         startRound(answer);
     } else {
         alert("Wrong =(");
@@ -30,5 +59,7 @@ function checkAnswer(el) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+    var stats = getStats();
+    var game_state = getGameState(stats.lastPlayed, data);
     startRound(first);
 });
