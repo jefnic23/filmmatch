a819541072dbdb3f8a0699a35e5d95b77ebfc795
Jeff <jefnic23@gmail.com>
2022-04-08 01:56:14 -0400

    local storage changes

--- app/static/main.js
+++ app/static/main.js
@@ -3,36 +3,30 @@ const answers = document.getElementById("answers");
 
 function getStats() {
     let statistics = localStorage.getItem("statistics");
-    return statistics == true ? JSON.parse(statistics) : {'lastPlayed': "Never", 'daysPlayed': 0, 'rightAnswers': 0, 'avgAnswers': 0, "totalLength": 0, "avgPct": 0};
+    return !statistics ? {'lastPlayed': "Never", 'daysPlayed': 0, 'rightAnswers': 0, 'avgAnswers': 0} : JSON.parse(statistics);
 }
 
-function getGameState(lastPlayed, data) {
+function getGameState() {
     let game_state = localStorage.getItem("game_state");
-    return (!game_state || compDay() > lastPlayed) ? {"gameLength": data.length, "answers": 0, "compPct": 0, "status": "in_progress"} : JSON.parse(game_state);
+    return !game_state ? {"answers": 0, "status": "in_progress"} : JSON.parse(game_state);
 }
 
-function updateStats(stats, lastPlayed, rightAnswers, gameLength) {
+function updateStats(stats, lastPlayed, rightAnswers) {
     stats.lastPlayed = lastPlayed;
     stats.daysPlayed++;
     stats.rightAnswers += rightAnswers;
     stats.avgAnswers = stats.rightAnswers / stats.daysPlayed;
-    stats.totalLength += gameLength;
-    stats.avgPct = stats.rightAnswers / stats.totalLength;
     return localStorage.setItem("statistics", JSON.stringify(stats));
 }
 
 function updateGameState(game_state, answers, stat) {
     game_state.answers = answers;
-    game_state.compPct = answers / game_state.gameLength;
     game_state.status = stat;
     return localStorage.setItem("game_state", JSON.stringify(game_state));
 }
 
-function compDay() {
-    let today = new Date()
-    let offset = today.getTimezoneOffset();
-    today = new Date(today.getTime() - (offset*60*1000));
-    return today.toISOString().split('T')[0];
+function compDay(serv_day, lastPlayed) {
+    return serv_day.toISOString().split('T')[0] > new Date(lastPlayed).toISOString().split('T')[0] ? true : false;
 }
 
 function startRound(c) {
@@ -49,7 +43,7 @@ function displayCurrent(c) {
 }
 
 function updateScore() {
-    return current.children[0].innerHTML = parseInt(current.children[0].innerHTML) + 1;
+    return current.children[0].innerHTML = parseInt(current.children[0].innerHTML)++;
 }
 
 function getAnswer(guess) {
@@ -65,14 +59,18 @@ function checkAnswer(el) {
     } else {
         alert("Wrong =(");
         let stats = getStats();
-        let game_state = getGameState(stats.lastPlayed, data);
-        updateStats(stats, compDay(), parseInt(current.children[0].innerHTML), data.length);
+        let game_state = getGameState();
+        updateStats(stats, serv_day, parseInt(current.children[0].innerHTML));
         updateGameState(game_state, parseInt(current.children[0].innerHTML), "complete")
     }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     var stats = getStats();
-    var game_state = getGameState(stats.lastPlayed, data);
-    startRound(first);
+    var game_state = getGameState();
+    if (stats.lastPlayed === "Never" || compDay(serv_day, stats.lastPlayed) || game_state.status === "in_progress") {
+        startRound(first);
+    } else {
+        console.log(stats, game_state); //display game results
+    }
 });
